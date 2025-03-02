872f0ffb3112bcbcd756e315c0f157a84dd0018c
WaqarNiyazi <niyazi.waqar007@gmail.com>
2025-03-03 00:45:25 +0530

    Deploy

--- apps/backend/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser, signupUser } from "./auth";
-
-const router = express.Router();
-
-router.post("/login", loginUser);
-router.post("/signup", signupUser);
-
-export default router;
