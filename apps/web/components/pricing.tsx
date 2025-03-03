0934ec11afa28325b5c2a232b6958135d9daeb50
WaqarNiyazi <niyazi.waqar007@gmail.com>
2025-03-03 23:38:28 +0530

    Deploy28

--- apps/web/components/pricing.tsx
+++ apps/web/components/pricing.tsx
@@ -160,7 +160,6 @@ export function Pricing({
                       minimumFractionDigits: 0,
                       maximumFractionDigits: 0,
                     }}
-                    formatter={(value) => `$${value}`}
                     transformTiming={{
                       duration: 500,
                       easing: "ease-out",
